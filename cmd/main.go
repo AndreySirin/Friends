516dcf95745ad61c4d97fd7ada4a519182704804
sirinAndrey <an.sirin@yandex.ru>
2024-11-03 14:22:49 +0300

    with table with prigogin

--- cmd/main.go
+++ cmd/main.go
@@ -8,17 +8,17 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+// TODO вынести переменные в config.YAML
 const (
 	address  = "127.0.0.1:5432"
 	username = "myuser"
 	password = "mypassword"
 	database = "mydatabase"
 
-	httpPort = ":8080"
+	httpPort = ":8000"
 )
 
 func main() {
-
 	lg := logg.New()
 	lg.Info("start server")
 
@@ -35,18 +35,19 @@ func main() {
 				"error", err)
 		}
 	}()
-
+	//TODO прикрутить нормальную миграцию https://github.com/rubenv/sql-migrate
 	if err = psql.DummyMigration(context.Background()); err != nil {
 		lg.Error("Failed to migrate",
 			"error", err)
 		return
 	}
 
-	httpServer := server.NewServer(lg, httpPort)
+	httpServer := server.NewServer(lg, httpPort, psql)
 	if err := httpServer.Run(); err != nil {
 		lg.Error("Server failed to start", err)
 		return
 	}
 	lg.Info("Shutting down")
+	//TODO реализовать gracefull shutdown
 
 }
---
+++ storage/dto.go
@@ -0,0 +1,10 @@
+package storage
+
+type ProductFriend struct {
+	ID          int
+	Name        string
+	Hobby       string
+	Price       int
+	ImageData   []byte
+	ImageBase64 string
+}
