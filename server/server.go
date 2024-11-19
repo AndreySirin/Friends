7d96be76752ab594ad0471eef2f85b4732e07a5c
sirinAndrey <an.sirin@yandex.ru>
2024-11-19 12:08:55 +0300

    migrite,config.yaml 2.0 cleaned code new html file "PostNewUser" "DeleteUser" graceful shutdown

--- server/server.go
+++ server/server.go
@@ -10,6 +10,7 @@ import (
 	"log/slog"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type zeter interface {
@@ -118,4 +119,15 @@ func (s *Server) Run() error {
 
 	s.log.Info(fmt.Sprintf("Listening on %s", s.server.Addr))
 	return s.server.ListenAndServe()
+}
+
+func (s *Server) ShutDown() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := s.server.Shutdown(ctx)
+	if err != nil {
+		s.log.Error("error shutting down server", "error", err)
+		return err
+	}
+	return nil
 }
